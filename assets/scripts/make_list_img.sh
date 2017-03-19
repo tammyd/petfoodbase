0ddb453b5507c5794399ba57b75d4457dd5b9fcb
Tammy Delahaye <tammyd@ea.com>
2017-03-18 17:38:12 -0700

    Removed templates and assets

--- assets/scripts/make_list_img.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-IN=$1
-OUT=$2
-#TMP2="working2.png"
-#CMD="convert $1 -trim cups-chicken-salmon.png working.png"
-CMD="convert $IN -trim  -resize 200x133 -background transparent -gravity center -extent 200x133 $OUT"
-echo $CMD
-eval $CMD
