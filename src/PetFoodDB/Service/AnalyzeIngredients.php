a6a1958d7e20107a0ef1e6ec250ce471113f9a3c
Tammy D <tammyd@gmail.com>
2020-10-10 14:15:01 -0700

    updated adjectives

--- src/PetFoodDB/Service/AnalyzeIngredients.php
+++ src/PetFoodDB/Service/AnalyzeIngredients.php
@@ -19,7 +19,8 @@ class AnalyzeIngredients
         return ['deboned', 'fresh deboned', 'organic', 'boneless', 'boneless/skinless', 'de-boned', 'whole', 'fresh', 'hydrolyzed', 'raw',
             'lamb', 'cutlets', 'flaked', 'shredded', 'flakes', 'freeze-dried', 'dried', 'baby', 'meat', 'dehydrated', 'fresh whole',
             'whole atlantic', 'dehydrated whole', 'fresh angus', 'fresh plains', 'fresh yorkshire', 'fresh whole pacific', 'grass-fed',
-            'raw grass-fed', 'finely ground', 'king', 'ocean caught', 'humanely raised', 'pasture raised', 'wild caught', 'sustainably sourced'];
+            'raw grass-fed', 'finely ground', 'king', 'ocean caught', 'humanely raised', 'pasture raised', 'wild caught', 'sustainably sourced',
+            'humanely raised', 'wild pacific', 'wild atlantic'];
     }
 
     static function getProteinSpecifics() {
